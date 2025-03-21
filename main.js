112f9230a3e32c1a018ac9e1a01250b9f563a2c9
Dmitry <dmitry2595@gmail.com>
2025-03-21 13:33:57 +0300

    refactor: del comit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ console.log('main.js conected');
 
 const themeBtn = document.querySelector('.rounded.theme');
 const themeIcon = themeBtn.querySelector("img");
-console.log(themeBtn);
+
 themeBtn.addEventListener('click', () => {
     document.body.classList.toggle("dark-theme");
     if (document.body.classList.contains("dark-theme")) {
